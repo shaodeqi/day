0369e4dc1ae60ceb817f2cfd58ca1f6a2622d680
邵德奇 <47845765+shaodeqi@users.noreply.github.com>
2022-04-10 00:26:32 +0800

    Update date.vue

--- src/components/date.vue
+++ src/components/date.vue
@@ -10,7 +10,10 @@ dayjs.extend(relativeTime);
 dayjs.locale('zh-cn');
 
 const knowDate = '2022-3-14 20:00';
-
+const durationTime = ref(+new Date() - +new Date(knowDate));
+setInterval(() => {
+  durationTime.value = +new Date() - +new Date(knowDate);
+}, 500);
 defineProps({
   msg: String,
 });
@@ -19,7 +22,55 @@ const count = ref(0);
 </script>
 
 <template>
-  <div>{{ dayjs(knowDate).toNow(true) }}</div>
+  <div class="date-wrap">
+    <div>与你相识</div>
+    <div>
+      <span v-if="dayjs.duration(durationTime).years()" class="count"
+        >{{ dayjs.duration(durationTime).years() }} <span class="unit">年</span>
+      </span>
+      <span v-if="dayjs.duration(durationTime).months()" class="count"
+        >{{ dayjs.duration(durationTime).months() }}
+        <span class="unit">年</span>
+      </span>
+      <span v-if="dayjs.duration(durationTime).days()" class="count"
+        >{{ dayjs.duration(durationTime).days() }}<span class="unit">天</span>
+      </span>
+      <span v-if="dayjs.duration(durationTime).hours()" class="count"
+        >{{ dayjs.duration(durationTime).hours()
+        }}<span class="unit">小时</span>
+      </span>
+      <span v-if="dayjs.duration(durationTime).minutes()" class="count"
+        >{{ dayjs.duration(durationTime).minutes()
+        }}<span class="unit">分钟</span>
+      </span>
+      <span v-if="dayjs.duration(durationTime).seconds()" class="count"
+        >{{ dayjs.duration(durationTime).seconds()
+        }}<span class="unit">秒</span>
+      </span>
+    </div>
+    <div class="shici">
+      「<span id="jinrishici-sentence">正在加载今日诗词....</span>」
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.date-wrap {
+  width: 100%;
+  margin-top: 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+}
+.count {
+  font-size: 26px;
+}
+.unit {
+  font-size: 20px;
+}
+.shici {
+  margin-top: 100px;
+}
+</style>
