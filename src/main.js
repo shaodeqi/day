a653d327db104e8f7fd30a68eb61b746f6db18a4
shaodeqi <shaodeqi@shaodeqideMacBook-Pro.local>
2022-05-19 16:09:12 +0800

    支持离线缓存

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import { createApp } from "vue";
+import { registerSW } from "virtual:pwa-register";
+
+if ("serviceWorker" in navigator) {
+  // && !/localhost/.test(window.location)) {
+  registerSW();
+}
 
 import App from "./app.vue";
-const app = createApp(App)
+const app = createApp(App);
 // app.use(VueAwesomeSwiper);
-app.mount("#app")
+app.mount("#app");
